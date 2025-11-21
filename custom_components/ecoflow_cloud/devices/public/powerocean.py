9086f9c59864be8457df1036a1411566dc627e6a
Kilian von Pflugk <github@jumoog.io>
2025-11-21 12:14:07 +0100

    more

--- custom_components/ecoflow_cloud/devices/public/powerocean.py
+++ custom_components/ecoflow_cloud/devices/public/powerocean.py
@@ -3,16 +3,17 @@ from .. import BaseDevice
 from .data_bridge import to_plain
 
 from ...sensor import (
-    CelsiusSensorEntity,
     CyclesSensorEntity,
+    EnergySensorEntity,
     VoltSensorEntity,
     WattsSensorEntity,
     LevelSensorEntity,
     AmpSensorEntity,
     TempSensorEntity,
+    MiscSensorEntity,
+    MilliVoltSensorEntity,
     SolarPowerSensorEntity,
     SolarAmpSensorEntity,
-    SystemPowerSensorEntity,
     StatusSensorEntity,
 )
 from ...entities import (
@@ -22,8 +23,33 @@ from ...entities import (
     BaseSelectEntity,
 )
 
+import base64
 import logging
 from typing import Any
+
+
+class ScaledEnergySensorEntity(EnergySensorEntity):
+    def __init__(
+        self,
+        client,
+        device,
+        mqtt_key,
+        title,
+        scale: float,
+        enabled: bool = True,
+        auto_enable: bool = False,
+    ):
+        super().__init__(client, device, mqtt_key, title, enabled, auto_enable)
+        self._scale = scale
+
+    def _update_value(self, val: Any) -> bool:
+        try:
+            scaled = int(val) * self._scale
+        except (TypeError, ValueError):
+            return False
+        if scaled < 0:
+            return False
+        return BaseSensorEntity._update_value(self, scaled)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,44 +60,71 @@ class PowerOcean(BaseDevice):
 
     def sensors(self, client: EcoflowApiClient) -> list[BaseSensorEntity]:
         return [
-            SolarPowerSensorEntity(client, self, "mpptPwr", "mpptPwr"),
+            #SolarPowerSensorEntity(client, self, "mpptPwr", "mpptPwr"),
             LevelSensorEntity(client, self, "96_8.bpSoc", "bpSoc"),
-            WattsSensorEntity(client, self, "bpPwr", "bpPwr"),
-            SystemPowerSensorEntity(client, self, "sysLoadPwr", "sysLoadPwr"),
-            SystemPowerSensorEntity(client, self, "sysGridPwr", "sysGridPwr"),
+            #WattsSensorEntity(client, self, "bpPwr", "bpPwr"),
+            #SystemPowerSensorEntity(client, self, "sysLoadPwr", "sysLoadPwr"),
+            #SystemPowerSensorEntity(client, self, "sysGridPwr", "sysGridPwr"),
 
 
-            LevelSensorEntity(client, self, "bp1.bpSoc", "Battery 1 SoC"),
-            WattsSensorEntity(client, self, "bp1.bpPwr", "Battery 1 Power"),
-            AmpSensorEntity(client, self, "bp1.bpAmp", "Battery 1 Current"),
-            VoltSensorEntity(client, self, "bp1.bpVol", "Battery 1 Voltage"),
-            TempSensorEntity(client, self, "bp1.bpHvMosTemp", "Battery 1 HV MOS Temp"),
-            TempSensorEntity(client, self, "bp1.bpLvMosTemp", "Battery 1 LV MOS Temp"),
-            TempSensorEntity(client, self, "bp1.bpEnvTemp", "Battery 1 Env Temp"),
-            TempSensorEntity(client, self, "bp1.bpHtsTemp", "Battery 1 Heater Temp"),
-            TempSensorEntity(client, self, "bp1.bpBusPosTemp", "Battery 1 Bus + Temp"),
-            TempSensorEntity(client, self, "bp1.bpBusNegTemp", "Battery 1 Bus - Temp"),
-            TempSensorEntity(client, self, "bp1.bpPtcTemp", "Battery 1 PTC Temp"),
-            TempSensorEntity(client, self, "bp1.bpPtcTemp2", "Battery 1 PTC Temp 2"),
-            TempSensorEntity(client, self, "bp1.bpMaxCellTemp", "Battery 1 Max Cell Temp"),
-            TempSensorEntity(client, self, "bp1.bpMinCellTemp", "Battery 1 Min Cell Temp"),
-            CyclesSensorEntity(client, self, "bp1.bpCycles", "Battery 1 Cycles"),
+            LevelSensorEntity(client, self, "bp1.bpSoc", "bp1Soc"),
+            WattsSensorEntity(client, self, "bp1.bpPwr", "bp1Pwr"),
+            AmpSensorEntity(client, self, "bp1.bpAmp", "bp1Amp"),
+            VoltSensorEntity(client, self, "bp1.bpVol", "bp1Vol"),
+            MilliVoltSensorEntity(client, self, "bp1.bpCellMaxVol", "bp1CellMaxVol"),
+            MilliVoltSensorEntity(client, self, "bp1.bpCellMinVol", "bp1MinCellVol"),
+            VoltSensorEntity(client, self, "bp1.bpBusVol", "bp1BusVol"),
+            TempSensorEntity(client, self, "bp1.bpHvMosTemp", "bp1HvMosTemp"),
+            TempSensorEntity(client, self, "bp1.bpLvMosTemp", "bp1LvMosTemp"),
+            TempSensorEntity(client, self, "bp1.bpEnvTemp", "bp1EnvTemp"),
+            TempSensorEntity(client, self, "bp1.bpHtsTemp", "bp1HtsTemp"),
+            TempSensorEntity(client, self, "bp1.bpBusPosTemp", "bp1BusPosTemp"),
+            TempSensorEntity(client, self, "bp1.bpBusNegTemp", "bp1BusNegTemp"),
+            TempSensorEntity(client, self, "bp1.bpPtcTemp", "bp1PtcTemp"),
+            TempSensorEntity(client, self, "bp1.bpPtcTemp2", "bp1PtcTemp2"),
+            TempSensorEntity(client, self, "bp1.bpMaxCellTemp", "bp1MaxCellTemp"),
+            TempSensorEntity(client, self, "bp1.bpMinCellTemp", "bp1MinCellTemp"),
+            TempSensorEntity(client, self, "bp1.bpMaxMosTemp", "bp1MaxMosTemp"),
+            TempSensorEntity(client, self, "bp1.bpMinMosTemp", "bp1MinMosTemp"),
+            CyclesSensorEntity(client, self, "bp1.bpCycles", "bp1Cycles"),
+            EnergySensorEntity(client, self, "bp1.bpRemainWatth", "bp1RemainWatth"),
+            ScaledEnergySensorEntity(client, self, "bp1.bpAccuChgEnergy", "bp1AccuChgEnergy", 0.01),
+            ScaledEnergySensorEntity(client, self, "bp1.bpAccuDsgEnergy", "bp1AccuDsgEnergy", 0.01),
+            MiscSensorEntity(client, self, "bp1.bpSnDecoded", "bp1Sn"),
+            MiscSensorEntity(client, self, "bp1.bmsChgDsgSta", "bp1bmsChgDsgSta"),
+            MiscSensorEntity(client, self, "bp1.dabModSta", "bp1dabModSta"),
+            MiscSensorEntity(client, self, "bp1.bpSysState", "bp1SysState"),
+            MiscSensorEntity(client, self, "bp1.bpRunSta", "bp1RunSta"),
 
-            LevelSensorEntity(client, self, "bp2.bpSoc", "Battery 2 SoC"),
-            WattsSensorEntity(client, self, "bp2.bpPwr", "Battery 2 Power"),
-            AmpSensorEntity(client, self, "bp2.bpAmp", "Battery 2 Current"),
-            TempSensorEntity(client, self, "bp2.bpHvMosTemp", "Battery 2 HV MOS Temp"),
-            TempSensorEntity(client, self, "bp2.bpLvMosTemp", "Battery 2 LV MOS Temp"),
-            TempSensorEntity(client, self, "bp2.bpEnvTemp", "Battery 2 Env Temp"),
-            TempSensorEntity(client, self, "bp2.bpHtsTemp", "Battery 2 Heater Temp"),
-            TempSensorEntity(client, self, "bp2.bpBusPosTemp", "Battery 2 Bus + Temp"),
-            TempSensorEntity(client, self, "bp2.bpBusNegTemp", "Battery 2 Bus - Temp"),
-            TempSensorEntity(client, self, "bp2.bpPtcTemp", "Battery 2 PTC Temp"),
-            TempSensorEntity(client, self, "bp2.bpPtcTemp2", "Battery 2 PTC Temp 2"),
-            TempSensorEntity(client, self, "bp2.bpMaxCellTemp", "Battery 2 Max Cell Temp"),
-            TempSensorEntity(client, self, "bp2.bpMinCellTemp", "Battery 2 Min Cell Temp"),
-            VoltSensorEntity(client, self, "bp2.bpVol", "Battery 2 Voltage"),
-            CyclesSensorEntity(client, self, "bp2.bpCycles", "Battery 2 Cycles"),
+
+            LevelSensorEntity(client, self, "bp2.bpSoc", "bp2Soc"),
+            WattsSensorEntity(client, self, "bp2.bpPwr", "bp2Pwr"),
+            AmpSensorEntity(client, self, "bp2.bpAmp", "bp2Amp"),
+            VoltSensorEntity(client, self, "bp2.bpVol", "bp2Vol"),
+            MilliVoltSensorEntity(client, self, "bp2.bpCellMaxVol", "bp2CellMaxVol"),
+            MilliVoltSensorEntity(client, self, "bp2.bpCellMinVol", "bp2CellMinVol"),
+            VoltSensorEntity(client, self, "bp2.bpBusVol", "bp2BusVol"),
+            TempSensorEntity(client, self, "bp2.bpHvMosTemp", "bp2HvMosTemp"),
+            TempSensorEntity(client, self, "bp2.bpLvMosTemp", "bp2LvMosTemp"),
+            TempSensorEntity(client, self, "bp2.bpEnvTemp", "bp2EnvTemp"),
+            TempSensorEntity(client, self, "bp2.bpHtsTemp", "bp2HtsTemp"),
+            TempSensorEntity(client, self, "bp2.bpBusPosTemp", "bp2BusPosTemp"),
+            TempSensorEntity(client, self, "bp2.bpBusNegTemp", "bp2BusNegTemp"),
+            TempSensorEntity(client, self, "bp2.bpPtcTemp", "bp2PtcTemp"),
+            TempSensorEntity(client, self, "bp2.bpPtcTemp2", "bp2PtcTemp2"),
+            TempSensorEntity(client, self, "bp2.bpMaxCellTemp", "bp2MaxCellTemp"),
+            TempSensorEntity(client, self, "bp2.bpMinCellTemp", "bp2MinCellTemp"),
+            TempSensorEntity(client, self, "bp2.bpMaxMosTemp", "bp2MaxMosTemp"),
+            TempSensorEntity(client, self, "bp2.bpMinMosTemp", "bp2MinMosTemp"),
+            CyclesSensorEntity(client, self, "bp2.bpCycles", "bp2Cycles"),
+            EnergySensorEntity(client, self, "bp2.bpRemainWatth", "bp2RemainWatth"),
+            ScaledEnergySensorEntity(client, self, "bp2.bpAccuChgEnergy", "bp2AccuChgEnergy", 0.01),
+            ScaledEnergySensorEntity(client, self, "bp2.bpAccuDsgEnergy", "bp2AccuDsgEnergy", 0.01),
+            MiscSensorEntity(client, self, "bp2.bpSnDecoded", "bp2Sn"),
+            MiscSensorEntity(client, self, "bp2.bmsChgDsgSta", "bp2bmsChgDsgSta"),
+            MiscSensorEntity(client, self, "bp2.dabModSta", "bp2dabModSta"),
+            MiscSensorEntity(client, self, "bp2.bpSysState", "bp2SysState"),
+            MiscSensorEntity(client, self, "bp2.bpRunSta", "bp2RunSta"),
 
             # String 1
             SolarPowerSensorEntity(
@@ -126,6 +179,7 @@ class PowerOcean(BaseDevice):
             WattsSensorEntity(
                 client, self, "96_1.pcsCPhase.apparentPwr", "pcsCPhase.apparentPwr"
             ),
+ 
             StatusSensorEntity(client, self),
         ]
 
@@ -191,3 +245,12 @@ class PowerOcean(BaseDevice):
             prefix = f"bp{int(dsrc)}"
             for field, value in entry.items():
                 params[f"{prefix}.{field}"] = value
+
+            bp_sn = entry.get("bpSn")
+            if isinstance(bp_sn, str):
+                try:
+                    decoded_sn = base64.b64decode(bp_sn).decode("utf-8")
+                except Exception:  # noqa: BLE001
+                    decoded_sn = None
+                if decoded_sn:
+                    params[f"{prefix}.bpSnDecoded"] = decoded_sn
